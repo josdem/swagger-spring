3146039a335f08ed53073089dfcb8fda00c4fc9a
josdem <joseluis.delacruz@gmail.com>
2016-07-16 21:06:01 -0500

    Adding Swagger configuration

--- src/main/groovy/com/jos/dem/swagger/initializer/ApplicationInitializer.groovy
+++ src/main/groovy/com/jos/dem/swagger/initializer/ApplicationInitializer.groovy
@@ -8,12 +8,14 @@ import org.springframework.web.servlet.DispatcherServlet
 import javax.servlet.ServletException
 
 import com.jos.dem.swagger.config.AppConfig
+import com.jos.dem.swagger.config.ApplicationSwaggerConfig
 
 class ApplicationInitializer implements WebApplicationInitializer {
 
   void onStartup(ServletContext servletContext) throws ServletException {
     AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext()
     ctx.register(AppConfig.class)
+    ctx.register(ApplicationSwaggerConfig.class)
     ctx.setServletContext(servletContext)
     Dynamic dynamic = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx))
     dynamic.addMapping("/")
