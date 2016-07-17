04703e839f55f822f660d71ca9b08339c38a5ad5
josdem <joseluis.delacruz@gmail.com>
2016-07-17 14:35:36 -0500

    Adding swagger configuration and dependencies

--- boot-configuration/src/main/groovy/com/jos/dem/swagger/controller/UserController.groovy
+++ boot-configuration/src/main/groovy/com/jos/dem/swagger/controller/UserController.groovy
@@ -1,14 +1,43 @@
 package com.jos.dem.swagger.controller
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET
+import static org.springframework.web.bind.annotation.RequestMethod.POST
+
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiImplicitParam
+import io.swagger.annotations.ApiImplicitParams
 
+import com.jos.dem.swagger.service.UserService
+import com.jos.dem.swagger.command.UserCommand
+import com.jos.dem.swagger.model.User
+
+@Api(description="knows how receive manage user requests")
 @RestController
-class UserController{
+@RequestMapping("/users/*")
+class UserController {
 
-  @RequestMapping("/")
-  String index(){
-    'Hello World!'
+  @Autowired
+  UserService userService
+
+  @ApiImplicitParams([
+  @ApiImplicitParam(name = "uuid", value = "User's uuid", required = true, dataType = "string", paramType = "query")
+  ])
+  @RequestMapping(method = GET, value = "/")
+  User getUserByUuid(UserCommand command){
+    userService.getByUuid(command.uuid)
+  }
+
+  @RequestMapping(method = POST, consumes="application/json")
+  User create(@RequestBody UserCommand command){
+    userService.create(command)
   }
 
 }
+
