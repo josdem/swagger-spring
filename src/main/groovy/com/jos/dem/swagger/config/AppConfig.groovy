30e37ccb25b7ff7331c5a73df5a5d9d34e8aecac
josdem <joseluis.delacruz@gmail.com>
2016-07-16 19:49:04 -0500

    Changing to groovy

---
+++ src/main/groovy/com/jos/dem/swagger/config/AppConfig.groovy
@@ -0,0 +1,9 @@
+package com.jos.dem.swagger.config
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ComponentScan(basePackages ="com.jos.dem.swagger.controller")
+class AppConfig {
+}
--- src/main/java/com/jos/dem/swagger/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.jos.dem.swagger.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan({"com.jos.dem.swagger.controller"})
-public class AppConfig {
-}
