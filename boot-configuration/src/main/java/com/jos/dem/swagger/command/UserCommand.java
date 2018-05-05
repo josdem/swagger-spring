f7d47474275fd1e4ec3dbbe7fb792177ae95e96a
josdem <joseluis.delacruz@gmail.com>
2018-05-05 16:25:10 -0500

    Adding user command in java

---
+++ boot-configuration/src/main/java/com/jos/dem/swagger/command/UserCommand.java
@@ -0,0 +1,40 @@
+package com.jos.dem.swagger.command;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(value="UserCommand", description="Generic command from users")
+class UserCommand {
+  @ApiModelProperty(value = "User's uuid", allowableValues = "aphanumeric")
+  private String uuid;
+  @ApiModelProperty(value = "User's name", allowableValues = "text")
+  private String name;
+  @ApiModelProperty(value = "User's email", allowableValues = "email@domain")
+  private String email;
+
+  public void setUuid(String uuid){
+    this.uuid = uuid;
+  }
+
+  public String getUuid(){
+    return this.uuid;
+  }
+
+  public void setName(String name){
+    this.name = name;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public void setEmail(String email){
+    this.email = email;
+  }
+
+  public String getEmail(){
+    return this.email;
+  }
+
+}
+
--- boot-configuration/src/main/java/com/jos/dem/swagger/service/impl/UserServiceImpl.java
+++ boot-configuration/src/main/java/com/jos/dem/swagger/service/impl/UserServiceImpl.java
@@ -6,11 +6,11 @@ public interface UserService {
 
   private Map<User> users = new HashMap<User>();
 
-  User getByUuid(String uuid){
+  public User getByUuid(String uuid){
     return users.get(uuid) != null ? users.get(uuid) : new User();
   }
 
-  User create(UserCommand command){
+  public User create(UserCommand command){
     User user = new User(command.getUuid(), command.getName(), command.getEmail());
     users.put(command.getUuid(), user);
     return user;
