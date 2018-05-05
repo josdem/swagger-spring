70bd117c89f322cbaa6499af9a0ba6334675ea35
josdem <joseluis.delacruz@gmail.com>
2018-05-05 16:08:35 -0500

    Adding user model in java

---
+++ boot-configuration/src/main/java/com/jos/dem/swagger/model/User.java
@@ -0,0 +1,14 @@
+package com.jos.dem.swagger.model
+
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+class User {
+
+  private String uuid;
+  private String name;
+  private String email;
+
+}
+
