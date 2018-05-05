69e2120e3f50ecaf2292c77c9e5c0dd801ca2929
josdem <joseluis.delacruz@gmail.com>
2018-05-05 16:27:19 -0500

    Adding Swagger boot application in java

---
+++ boot-configuration/src/main/java/com/jos/dem/swagger/SwaggerBootApplication.java
@@ -0,0 +1,13 @@
+package com.jos.dem.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerBootApplication {
+
+	static void main(String[] args) {
+		return SpringApplication.run(SwaggerBootApplication.class, args);
+	}
+
+}
