199754cce242ae29a2fe5bf846c21dcd69854fe8
josdem <joseluis.delacruz@gmail.com>
2018-05-06 10:41:51 -0500

    Adding log to the controller

--- boot-configuration/src/main/java/com/jos/dem/swagger/controller/UserController.java
+++ boot-configuration/src/main/java/com/jos/dem/swagger/controller/UserController.java
@@ -21,27 +21,35 @@ import com.jos.dem.swagger.service.UserService;
 import com.jos.dem.swagger.command.UserCommand;
 import com.jos.dem.swagger.model.User;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Api(description="knows how receive manage user requests")
 @RestController
 @RequestMapping("/users/*")
 public class UserController {
 
+  private Logger log = LoggerFactory.getLogger(this.getClass());
+
   @Autowired
   private UserService userService;
 
-  @RequestMapping
+  @RequestMapping(method = GET)
   public List<User> getAll(){
+    log.info("Getting all users");
     return userService.getAll();
   }
 
   @ApiImplicitParam(name = "uuid", value = "User's uuid", required = true, dataType = "string", paramType = "query")
-  @RequestMapping(method = GET, value="{uuid}")
+  @RequestMapping(method = GET, value = "{uuid}")
   public User getByUuid(@PathVariable String uuid){
+    log.info("Getting user by uuid: " + uuid);
     return userService.getByUuid(uuid);
   }
 
   @RequestMapping(method = POST, consumes="application/json")
   public User create(@RequestBody UserCommand command){
+    log.info("Saving user: w/uuid: " + command.getUuid());
     return userService.create(command);
   }
 
