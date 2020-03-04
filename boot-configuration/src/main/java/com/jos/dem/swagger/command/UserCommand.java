15cd7685d6c6fa22c3944108af4f70eafcab63aa
josdem <joseluis.delacruz@gmail.com>
2020-03-04 10:37:29 -0500

    Adding annotations to controller and dto

--- boot-configuration/src/main/java/com/jos/dem/swagger/command/UserCommand.java
+++ boot-configuration/src/main/java/com/jos/dem/swagger/command/UserCommand.java
@@ -2,19 +2,23 @@ package com.jos.dem.swagger.command;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import javax.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@ApiModel(value="UserModel", description="Model who represents an user entity")
+@ApiModel(value = "UserModel", description = "Model who represents an user entity")
 public class UserCommand {
-  @ApiModelProperty(value = "User's uuid", allowableValues = "aphanumeric")
+  @ApiModelProperty(value = "User's uuid", required = true)
+  @NotBlank(message = "uuid is required")
   private String uuid;
-  @ApiModelProperty(value = "User's name", allowableValues = "text")
+
+  @ApiModelProperty(value = "User's name", required = true)
+  @NotBlank(message = "name is required")
   private String name;
-  @ApiModelProperty(value = "User's email", allowableValues = "email@domain")
+
+  @ApiModelProperty(value = "User's email", required = true)
+  @NotBlank(message = "email is required")
   private String email;
 }
-
-
--- boot-configuration/src/main/java/com/jos/dem/swagger/controller/UserController.java
+++ boot-configuration/src/main/java/com/jos/dem/swagger/controller/UserController.java
@@ -5,14 +5,22 @@ import com.jos.dem.swagger.model.User;
 import com.jos.dem.swagger.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-@Api(value="knows how receive manage user requests")
+@Api(tags = "knows how receive manage user requests")
 @RestController
 @RequestMapping("/users/*")
 @RequiredArgsConstructor
@@ -22,24 +30,38 @@ public class UserController {
 
   private final UserService userService;
 
+  @ApiOperation(value = "Getting all users")
+  @ApiResponses(
+      value = {
+        @ApiResponse(code = 200, message = "Getting all users"),
+        @ApiResponse(code = 400, message = "Bad request"),
+        @ApiResponse(code = 500, message = "Something went wrong")
+      })
   @GetMapping
-  public List<User> getAll(){
+  public List<User> getAll() {
     log.info("Getting all users");
     return userService.getAll();
   }
 
-  @ApiImplicitParam(name = "uuid", value = "User's uuid", required = true, dataType = "string", paramType = "path")
+  @ApiImplicitParams(
+      value = {
+        @ApiImplicitParam(
+            name = "uuid",
+            required = true,
+            dataType = "string",
+            paramType = "path",
+            value = "User's uuid")
+      })
   @GetMapping(value = "{uuid}")
-  public User getByUuid(@PathVariable String uuid){
+  public User getByUuid(@PathVariable String uuid) {
     log.info("Getting user by uuid: " + uuid);
     return userService.getByUuid(uuid);
   }
 
-  @PostMapping(consumes="application/json")
-  public User create(@RequestBody UserCommand command){
+  @PostMapping(consumes = "application/json")
+  public User create(@RequestBody UserCommand command) {
     log.info("Saving user: w/uuid: " + command.getUuid());
     return userService.create(command);
   }
 
 }
-
