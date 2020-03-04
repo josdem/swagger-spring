63cf451f93d98b241d7706bd617f6db8ceb74161
josdem <joseluis.delacruz@gmail.com>
2020-03-04 11:52:59 -0500

    Adding missing annotations

--- boot-configuration/src/main/java/com/jos/dem/swagger/controller/UserController.java
+++ boot-configuration/src/main/java/com/jos/dem/swagger/controller/UserController.java
@@ -10,20 +10,28 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import java.util.List;
+import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Api(tags = "knows how receive manage user requests")
+@Validated
 @RestController
-@RequestMapping("/users/*")
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
 
@@ -44,6 +52,13 @@ public class UserController {
     return userService.getAll();
   }
 
+  @ApiOperation(value = "Get user by uuid")
+  @ApiResponses(
+      value = {
+          @ApiResponse(code = 200, message = "Getting all users"),
+          @ApiResponse(code = 400, message = "Bad request"),
+          @ApiResponse(code = 500, message = "Something went wrong")
+      })
   @ApiImplicitParams(
       value = {
         @ApiImplicitParam(
@@ -53,16 +68,34 @@ public class UserController {
             paramType = "path",
             value = "User's uuid")
       })
-  @GetMapping(value = "{uuid}")
-  public User getByUuid(@PathVariable String uuid) {
+  @GetMapping(value = "/{uuid}")
+  public User getByUuid(
+      @PathVariable
+          @Pattern(
+              regexp =
+                  "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$",
+              message = "Invalid uuid format")
+          String uuid) {
     log.info("Getting user by uuid: " + uuid);
     return userService.getByUuid(uuid);
   }
 
+  @ApiOperation(value = "Get user by uuid")
+  @ApiResponses(
+      value = {
+          @ApiResponse(code = 200, message = "User created"),
+          @ApiResponse(code = 400, message = "Bad request"),
+          @ApiResponse(code = 500, message = "Something went wrong")
+      })
   @PostMapping(consumes = "application/json")
   public User create(@Valid @RequestBody UserCommand command) {
     log.info("Saving user: w/uuid: " + command.getUuid());
     return userService.create(command);
   }
 
+  @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(ConstraintViolationException.class)
+  public ResponseEntity<String> handleException(ConstraintViolationException ce) {
+    return new ResponseEntity<>(ce.getMessage(), HttpStatus.BAD_REQUEST);
+  }
 }
--- boot-configuration/src/main/java/com/jos/dem/swagger/service/impl/UserServiceImpl.java
+++ boot-configuration/src/main/java/com/jos/dem/swagger/service/impl/UserServiceImpl.java
@@ -1,20 +1,22 @@
 package com.jos.dem.swagger.service.impl;
 
-import java.util.Map;
-import java.util.List;
-import java.util.HashMap;
-import java.util.ArrayList;
-
-import com.jos.dem.swagger.model.User;
 import com.jos.dem.swagger.command.UserCommand;
+import com.jos.dem.swagger.model.User;
 import com.jos.dem.swagger.service.UserService;
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
   private final Map<String, User> users = new HashMap<>();
+
+  private Logger log = LoggerFactory.getLogger(this.getClass());
 
   public List<User> getAll(){
     return new ArrayList<>(users.values());
