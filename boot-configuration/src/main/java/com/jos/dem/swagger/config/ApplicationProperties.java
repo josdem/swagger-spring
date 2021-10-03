07ed28d9513777891e1e8056f8a8d0eae82c6efd
josdem <joseluis.delacruz@gmail.com>
2021-10-03 10:43:34 -0400

    Adding missing annotation

--- boot-configuration/src/main/java/com/jos/dem/swagger/config/ApplicationProperties.java
+++ boot-configuration/src/main/java/com/jos/dem/swagger/config/ApplicationProperties.java
@@ -3,9 +3,11 @@ package com.jos.dem.swagger.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
+@Component
 @ConfigurationProperties("api")
 public class ApplicationProperties {
     private String version;
