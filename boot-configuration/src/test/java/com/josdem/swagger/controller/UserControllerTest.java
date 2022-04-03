5f03aeb65b0f57d02254ffec192386e15ebc975f
josdem <joseluis.delacruz@gmail.com>
2022-04-03 17:57:37 -0400

    #1 Adding test case to create a new user

--- boot-configuration/src/test/java/com/josdem/swagger/controller/UserControllerTest.java
+++ boot-configuration/src/test/java/com/josdem/swagger/controller/UserControllerTest.java
@@ -1,5 +1,7 @@
 package com.josdem.swagger.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.josdem.swagger.model.User;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,7 +12,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.UUID;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Slf4j
@@ -20,6 +25,17 @@ public class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("it creates a new user")
+    void shouldCreateUser(TestInfo testInfo) throws Exception {
+        log.info("Running {}", testInfo.getDisplayName());
+        User user = new User();
+        user.setUuid(UUID.randomUUID());
+        user.setName("josdem");
+        user.setEmail("contact@josdem.io");
+        mockMvc.perform(post("/users").contentType(MediaType.APPLICATION_JSON).content(new ObjectMapper().writeValueAsString(user))).andExpect(status().isOk());
+    }
 
     @Test
     @DisplayName("it gets all users")
